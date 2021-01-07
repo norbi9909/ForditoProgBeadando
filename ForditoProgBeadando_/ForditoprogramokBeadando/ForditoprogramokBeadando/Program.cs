8633ec19247991349f6a3a6872920f26005d567b
norbi9909 <norbi9909@gmail.com>
2021-01-07 18:03:06 +0100

    Beadando

---
+++ ForditoProgBeadando_/ForditoprogramokBeadando/ForditoprogramokBeadando/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;   
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fordito
+{
+    public static class Program
+    {
+        //public static string[][] matrix = new string[6][];
+        public static string[,] matrix = new string[12, 7];
+        public static string[] sArray = new string[7];
+        public static List<string> sList = new List<string>();
+
+        public static Stack stack;
+
+        public static bool format(string matrixElem)
+        {
+            
+            if (matrixElem.Length == 0)
+            {
+                Console.WriteLine("0");
+                check = true;
+                return true;
+            }
+
+            if(matrixElem.Trim() == "elfogad")
+            {
+                Console.WriteLine("Elfogad"); 
+                check = true;
+                return true;
+            }
+
+            if(matrixElem.Trim() == "pop")
+            {
+                //stack.Pop();
+                input = input.Substring(1); //+1 vagy -1
+                //input = input.Substring(0) + input.Substring(i+1,input.Length); //+1 vagy -1
+                return false;
+            }
+
+            //string[] result = new string[2];
+
+            if (matrixElem.Contains('('))
+            {
+                string seged = matrixElem.Substring(1).Split(',')[0];
+                for (int j = seged.Length-1; j >= 0; j--)
+                {
+                    stack.Push(seged[j].ToString());
+                }
+            }
+
+            if (matrixElem.Contains(')'))
+            {
+                ruleNumber += matrixElem.Substring(0, matrixElem.Length - 1).Split(',')[1];
+            }  
+            
+            /*
+            stack.Push(matrixElem.Trim().Split(',')[0]);
+            sb.Append(matrixElem.Trim().Split(',')[1]);
+            ruleNumber = sb.ToString();*/
+            
+            return false;
+        }
+
+        /*
+        private void TrimBrackets(ref string element)
+        {
+            element = element.Substring(element.IndexOf('(') + 1);
+            element = element.Substring(0, element.IndexOf(')'));
+        }*/
+
+
+        public static void fillTheMatrix()
+        {
+            StreamReader sr = new StreamReader("rule.txt");
+            string helper = "";
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                helper += sr.ReadLine();
+                for (int k = 0; k < sArray.Length; k++)
+                {
+                    sArray[k] = helper.Split('|')[k];
+                }
+                //sArray = helper.Split('|');
+                //sList.AddRange(sr.ReadLine().Split('|').ToList());
+                for (int j = 0; j < sArray.Length; j++)
+                {
+                    //matrix[i][j] = sList[j];
+                    matrix[i, j] = sArray[j];
+
+                }
+                helper = "";
+                i++;
+            }
+        }
+
+        public static StringBuilder sb = new StringBuilder();
+        public static string ruleNumber = "";
+        public static string input = "(i+i)*i#";
+        public static bool check = false;
+
+        static void Main(string[] args)
+        {
+            fillTheMatrix();
+            string helper;
+            string[] elements = new string[2];
+            
+            stack = new Stack();
+            stack.Push("#");
+            stack.Push("E");
+            
+            do
+            {
+                //for (int i = 0; i < input.Length; i++)
+                //{
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (input[0].ToString() == matrix[0, j])
+                    {
+                        helper = stack.Pop().ToString();
+                        for (int t = 0; t < matrix.GetLength(0); t++)
+                        {
+                            if (helper == matrix[t, 0])
+                            {
+                                ///check = format(matrix[t, j]);
+                                format(matrix[t, j]);
+                                
+
+                            }
+                        }
+                    }
+                }
+                //}
+            }while(!check); /*while (input != "#" && stack.Count >= 0);*/
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(ruleNumber);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ForditoProgBeadando_/ForditoprogramokBeadando/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;   
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace fordito
+{
+    public static class Program
+    {
+        public static string[,] rulesMatrix = new string[12, 7];
+        public static string[] helperArray = new string[7];
+        public static Stack stack;
+
+        public static void initRules()
+        {
+            StreamReader sr = new StreamReader("rule.txt");
+            string helper = "";
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                helper += sr.ReadLine();
+                for (int k = 0; k < helperArray.Length; k++)
+                {
+                    helperArray[k] = helper.Split('|')[k];
+                }
+                for (int j = 0; j < helperArray.Length; j++)
+                {
+                    rulesMatrix[i, j] = helperArray[j];
+
+                }
+                helper = "";
+                i++;
+            }
+        }
+
+        public static bool checkInput(string input, string[,] matrix)
+        {
+            string characters = "";
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                characters += matrix[0, j];
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!characters.Contains(input[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static string simple(string st)
+        {
+            return Regex.Replace(st, @"([0-9]+)", "i");
+        }
+
+        public static bool reform(string eMatrix)
+        {
+            if (eMatrix.Length == 0)
+            {
+                Console.WriteLine("Nem megálló állapot!");
+                boolHelper = true;
+                return true;
+            }
+
+            if(eMatrix.Trim() == "elfogad")
+            {
+                Console.WriteLine("Elfogadva"); 
+                boolHelper = true;
+                return true;
+            }
+
+            if(eMatrix.Trim() == "pop")
+            {
+                actualInputElement = actualInputElement.Substring(1); 
+                return false;
+            }
+
+            if (eMatrix.Contains('('))
+            {
+                string subHelper = eMatrix.Substring(1).Split(',')[0];
+                
+                for (int j = subHelper.Length-1; j >= 0; j--)
+                {
+                    if (subHelper[j].Equals('e'))
+                    {
+                        continue;
+                    }
+                    stack.Push(subHelper[j].ToString());
+                }
+            }
+
+            if (eMatrix.Contains(')'))
+            {
+                ruleNo += eMatrix.Substring(0, eMatrix.Length - 1).Split(',')[1];
+            }
+            string stackHelper = "";
+            foreach (string e in stack)
+            {
+                stackHelper += e;
+            }
+
+            Console.WriteLine("{0}, {1}, {2}", actualInputElement, stackHelper, ruleNo);
+
+            return false;
+        }
+
+        public static StringBuilder sb = new StringBuilder();
+        public static string ruleNo = "";
+        public static string actualInputElement = "(203+304)*55#";
+        public static bool boolHelper = false;
+        public static string helper;
+        static void Main(string[] args)
+        {
+            initRules();
+            string[] elements = new string[2];
+            actualInputElement = simple(actualInputElement);
+            stack = new Stack();
+            stack.Push("#");
+            stack.Push("E");
+
+            Console.WriteLine("Rules: ");
+
+
+            Console.WriteLine();
+
+            for (int i = 0; i < rulesMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < rulesMatrix.GetLength(1); j++)
+                {
+                    Console.Write(rulesMatrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            if (checkInput(actualInputElement, rulesMatrix))
+            {
+                do
+                {
+                    for (int j = 0; j < rulesMatrix.GetLength(1); j++)
+                    {
+                        if (actualInputElement[0].ToString() == rulesMatrix[0, j])
+                        {
+                            helper = stack.Pop().ToString();
+                            for (int t = 0; t < rulesMatrix.GetLength(0); t++)
+                            {
+                                if (helper == rulesMatrix[t, 0])
+                                {
+                                    reform(rulesMatrix[t, j]);
+                                }
+                            }
+                        }
+                    }
+                } while (!boolHelper);
+            }
+            else
+            {
+                Console.WriteLine("Nem megfelelő input!");
+            }
+
+            Console.WriteLine("RuleNo: {0}", ruleNo);
+            Console.ReadLine();
+        }
+    }
+}
